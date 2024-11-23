d1b6aad03d1ab5de22f77a0cceb1b7351e040c41
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-11-23 14:40:33 +1300

    :clown_face: some tailwind config and experimentation
    successfully imports json from data and renders it tho

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,29 @@
 import type { Config } from "tailwindcss";
 
+// TODO: finalise the colours for the tailwind config
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                "wdcc-gray": "#4f4f4f",
+                "wdcc-orange": "#fb8500",
+                "wdcc-yellow": "#ffd166",
+                "wdcc-blue": {
+                    500: "#1a68ef",
+                    600: "#3a86ff",
+                },
+                "wdcc-navy": "#03045e",
+                "wdcc-indigo": "#7209b7",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
