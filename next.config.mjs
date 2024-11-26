c0e70e26fa11dbefca076960ba1cbc1c65f9d095
Tulza <Tulza0410@gmail.com>
2024-11-27 02:37:12 +1300

    change to next15 with payload

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
+import { withPayload } from "@payloadcms/next/withPayload";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     compiler: {
         removeConsole: process.env.NODE_ENV === "production",
     },
+    experimental: {
+        reactCompiler: true,
+    },
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
