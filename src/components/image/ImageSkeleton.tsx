758919f99e06c202684b8758778ac0fd2e1c584e
Tulza <Tulza0410@gmail.com>
2024-11-29 04:25:45 +1300

    🚑 hotfix

--- src/components/image/ImageSkeleton.tsx
+++ src/components/image/ImageSkeleton.tsx
@@ -4,7 +4,10 @@ import { motion } from "framer-motion";
 
 export const ImageSkeleton = ({ width, height }: { width: number; height: number }) => {
     return (
-        <div style={{ aspectRatio: `${width} / ${height}` }} className="grid size-full place-items-center border">
+        <div
+            style={{ aspectRatio: `${width} / ${height}` }}
+            className="grid size-full place-items-center rounded-[inherit] border"
+        >
             <motion.p key="loading" exit={{ opacity: 0 }} transition={{ duration: 0.1 }}>
                 Loading...
             </motion.p>
