a2413914781c37e27e94b378a7ce1402add43ccb
Nate Williamson <nwilliamson@atlassian.com>
2024-12-06 01:59:37 +1100

    :sparkles: add links to navbar

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,3 +1,43 @@
+import Link from "next/link";
+
+const links = [
+    { href: "/", label: "Home" },
+    { href: "/projects", label: "Projects" },
+    { href: "/events", label: "Events" },
+    { href: "/about", label: "About" },
+    { href: "/faq", label: "FAQ" },
+    { href: "https://go.wdcc.co.nz", label: "Linktree", external: true },
+];
+
 export default function Navbar() {
-    return <header className="w-full bg-blue-600">Navbar</header>;
+    return (
+        <header className="flex w-full justify-between bg-blue-600">
+            Navbar
+            <ul className="flex gap-10">
+                {links.map((link) => (
+                    <NavLink key={link.href} href={link.href} external={link.external}>
+                        {link.label}
+                    </NavLink>
+                ))}
+            </ul>
+        </header>
+    );
+}
+
+type NavLinkProps = {
+    href: string;
+    children: React.ReactNode;
+    external?: boolean;
+};
+
+function NavLink({ href, external, children }: NavLinkProps) {
+    const target = external ? "_blank" : undefined; // Open in new tab if external
+
+    return (
+        <li>
+            <Link href={href} target={target} rel="noopener noreferrer">
+                {children}
+            </Link>
+        </li>
+    );
 }
