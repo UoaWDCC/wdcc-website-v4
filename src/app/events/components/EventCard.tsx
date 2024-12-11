03093a992eb18d1a501faedb67c132bc3dc474cc
Nate Williamson <nwilliamson@atlassian.com>
2024-12-11 21:11:37 +1100

    :sparkles: create past events section

--- src/app/events/components/EventCard.tsx
+++ src/app/events/components/EventCard.tsx
@@ -1,3 +1,7 @@
-export default function EventCard() {
-    return <div>EventCard</div>;
+type EventCardProps = {
+    tempText: string;
+};
+
+export default function EventCard({ tempText }: EventCardProps) {
+    return <div className="center-content h-96 w-full bg-slate-300">{tempText}</div>;
 }
--- src/app/events/components/PastEvents.tsx
+++ src/app/events/components/PastEvents.tsx
@@ -1,3 +1,18 @@
+import Header from "@/components/primitives/Header";
+
+import EventCard from "./EventCard";
+
+const tempDummyEventText = ["Event 1", "Event 2", "Event 3", "Event 4", "Event 5", "Event 6", "Event 7", "Event 8"];
+
 export default function PastEvents() {
-    return <div>PastEvents</div>;
+    return (
+        <div className="px-36 py-20">
+            <Header>Past Events</Header>
+            <div className="mt-5 grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
+                {tempDummyEventText.map((tempText, i) => (
+                    <EventCard key={i} tempText={tempText} />
+                ))}
+            </div>
+        </div>
+    );
 }
