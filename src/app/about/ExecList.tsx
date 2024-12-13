0c39291f194a4a69db5c8e37cfa56b35b4d58374
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-12-13 16:53:53 +1300

    :hammer: combine divs because stupid

--- src/app/about/ExecList.tsx
+++ src/app/about/ExecList.tsx
@@ -12,16 +12,8 @@ export default function ExecList({ execs }: ExecListProps) {
         <div>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
                 {execs.map((exec) => (
-                    <div key={exec.name}>
-                        <div className="flex flex-1 items-center justify-center">
-                            <Image
-                                src={placeholder}
-                                alt={exec.name}
-                                width={200}
-                                height={200}
-                                className="h-auto w-full"
-                            />
-                        </div>
+                    <div key={exec.name} className="flex flex-1 items-center justify-center">
+                        <Image src={placeholder} alt={exec.name} width={200} height={200} className="h-auto w-full" />
                     </div>
                 ))}
             </div>
