74ffaed8b47ee86a8d6c86c3f9df04e01ad075b4
Kot <89110272+Kot6603@users.noreply.github.com>
2024-12-27 17:26:05 +1300

    feat(events): added a header to roadmap section

--- src/app/events/_components/EventsRoadmap.tsx
+++ src/app/events/_components/EventsRoadmap.tsx
@@ -1,3 +1,9 @@
+import SectionWithHeader from "@/components/SectionWithHeader";
+
 export default function EventsRoadmap() {
-    return <div className="center-content h-[40em] bg-slate-300">EventsRoadmap</div>;
+    return (
+        <SectionWithHeader headerText="2025 Event Roadmap">
+            <div className="center-content h-[40em] bg-slate-300 rounded-2xl">EventsRoadmap</div>
+        </SectionWithHeader>
+    );
 }
