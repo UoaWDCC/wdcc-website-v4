cce0c7aad6c5b0cc7b42bc3860db55ca4ac7dd58
Nate Williamson <nwilliamson@atlassian.com>
2024-12-28 12:17:12 +1100

    :lipstick: implement md-lg navbar responsiveness

--- src/components/navigation/navbar/NavigationBar.tsx
+++ src/components/navigation/navbar/NavigationBar.tsx
@@ -1,29 +1,34 @@
 import type { ClassName } from "@/types/utils";
 
+
+
 import WDCCLogo from "@/assets/svg/WDCCLogo";
 import { Button } from "@/components/primitives/Button";
 import { navigationLinkData } from "@/data/links/navigationLinks.data";
 import { cn } from "@/libs/utils";
 
+
+
 import { Anchor } from "../../primitives/Anchor";
-import { GenerateNavigationBarLinks } from "./GenerateNavigationLinks";
-import NavigationMenu from "./NavigationMenu";
+import { GenerateNavigationBarLinks } from "./NavigationLinks";
+import NavigationMenu from "./NavigationMobileMenu";
+
 
 const NavigationBar = ({ className }: ClassName) => {
     return (
         <div
-            className={cn("flex select-none flex-col items-center justify-between gap-4 px-16 py-6 sm:flex-row", className)}
+            className={cn("flex select-none flex-col items-center justify-between gap-4 px-16 py-4 lg:py-6 sm:flex-row", className)}
         >
             <WDCCLogo className="fill-black lg:block" />
-            <div className="flex items-center gap-12 font-semibold">
+            <div className="flex items-center gap-8 lg:gap-12 font-semibold">
                 {/* Links */}
-                <div className="hidden h-full items-center gap-12 md:flex">
+                <div className="hidden h-full items-center gap-8 lg:gap-12 md:flex">
                     <GenerateNavigationBarLinks links={navigationLinkData} />
                 </div>
                 {/* Vertical line */}
                 <div className="h-5 w-0.5 rounded bg-black" />
                 {/* Buttons */}
-                <div className="flex gap-3">
+                <div className="flex gap-2 lg:gap-3">
                     <Anchor set={{ type: "muted", rounded: true }} href="/socials  ">
                         <Button set={{ type: "secondary", color: "blue" }}>Our socials</Button>
                     </Anchor>
