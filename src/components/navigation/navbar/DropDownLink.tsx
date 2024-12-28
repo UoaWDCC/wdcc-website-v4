aa6ffd89f7756b7d970d8211606b770c2e866bc6
Nate Williamson <nwilliamson@atlassian.com>
2024-12-28 12:05:18 +1100

    :art: remove type assertion from dropdown link

--- src/components/navigation/navbar/DropDownLink.tsx
+++ src/components/navigation/navbar/DropDownLink.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, type HTMLAttributes } from "react";
+import { useState, type HTMLAttributes } from "react";
 import Link from "next/link";
 import { motion, type Variants } from "framer-motion";
 
@@ -24,7 +24,9 @@ const DropDown = ({ link, ...props }: DropdownProps) => {
     };
     return (
         // xd unreadable code sorry not sorry, can't fire me this way
+        // Jesus-
         <motion.div className="relative z-10 flex flex-col gap-4" onHoverStart={HoverOn} onHoverEnd={HoverOff}>
+            {/* Main link in navbar */}
             <div {...props} className={cn("relative z-10 flex items-center", props.className)}>
                 <p className="cursor-pointer whitespace-nowrap">
                     <span className={cn("inline-block -translate-x-1 transition-transform", open && "translate-x-0")}>
@@ -36,30 +38,33 @@ const DropDown = ({ link, ...props }: DropdownProps) => {
                     </span>
                 </p>
             </div>
-            <motion.div
-                initial="hide"
-                animate={open ? "show" : "hide"}
-                className="absolute z-50 flex flex-col whitespace-pre pt-8"
-                transition={{ staggerChildren: 1 }}
-                variants={containerVariant}
-            >
-                {link.drop!.map((link) => (
-                    <Link key={link.label} href={link.href}>
-                        <motion.div
-                            initial="hide"
-                            animate={open ? "show" : "hide"}
-                            className="group relative cursor-pointer transition-[padding-left] *:*:inline-block *:inline-block hover:pl-1"
-                        >
-                            {link.label.split("").map((char, i) => (
-                                <motion.span key={i} className="overflow-hidden">
-                                    <motion.span variants={LetterVariant}>{char}</motion.span>
-                                </motion.span>
-                            ))}
-                            <div className="absolute bottom-0 left-0 h-0.5 w-0 bg-black transition-[width] group-hover:w-1/2" />
-                        </motion.div>
-                    </Link>
-                ))}
-            </motion.div>
+            {/* Dropdown links */}
+            {link.drop && (
+                <motion.div
+                    initial="hide"
+                    animate={open ? "show" : "hide"}
+                    className="absolute z-50 flex flex-col whitespace-pre pt-8"
+                    transition={{ staggerChildren: 1 }}
+                    variants={containerVariant}
+                >
+                    {link.drop.map((link) => (
+                        <Link key={link.label} href={link.href}>
+                            <motion.div
+                                initial="hide"
+                                animate={open ? "show" : "hide"}
+                                className="group relative cursor-pointer transition-[padding-left] *:*:inline-block *:inline-block hover:pl-1"
+                            >
+                                {link.label.split("").map((char, i) => (
+                                    <motion.span key={i} className="overflow-hidden">
+                                        <motion.span variants={LetterVariant}>{char}</motion.span>
+                                    </motion.span>
+                                ))}
+                                <div className="absolute bottom-0 left-0 h-0.5 w-0 bg-black transition-[width] group-hover:w-1/2" />
+                            </motion.div>
+                        </Link>
+                    ))}
+                </motion.div>
+            )}
         </motion.div>
     );
 };
