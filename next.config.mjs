d65600b1e328a8eb4a098a52ca002f6f2211d542
Nate Williamson <nwilliamson@atlassian.com>
2024-12-28 19:27:17 +1100

    Revert ":wrench: configure Next.js to use absolute imports at src/ folder"
    This reverts commit 3a6b9c8aa2af3fda4c9219eed5829796ffa1c1f0.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
-
-
-
-
 const bundleAnalyzer = withBundleAnalyzer({
     enabled: process.env.ANALYZE === "true",
 });
@@ -12,9 +8,6 @@ const bundleAnalyzer = withBundleAnalyzer({
 const nextConfig = {
     compiler: {
         removeConsole: process.env.NODE_ENV === "production",
-    },
-    compilerOptions: {
-        baseUrl: "src/",
     },
     images: {
         remotePatterns: [
