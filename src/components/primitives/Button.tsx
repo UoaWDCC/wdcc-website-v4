9afb742b07b7b91c7e3f849d7f56f2a1bc20cdca
Nate Williamson <nwilliamson@atlassian.com>
2024-12-28 19:07:51 +1100

    :bricks: type button props rigourously (hopefully)

--- src/components/primitives/Button.tsx
+++ src/components/primitives/Button.tsx
@@ -1,142 +1,149 @@
 "use client";
 
-import { ButtonHTMLAttributes, ReactNode } from "react";
-// import { HTMLMotionProps, motion, MotionProps } from "framer-motion";
+import { ButtonHTMLAttributes, LinkHTMLAttributes, ReactNode } from "react";
 import Link from "next/link";
 import { tv, type VariantProps } from "tailwind-variants";
 
 const button = tv({
-        base: "rounded-full py-2 px-6 font-bold flex justify-center items-center transition duration-200 w-fit hover:cursor-pointer",
-        variants: {
-            variant: {
-                primary: "border-2",
-                secondary: "",
-                tertiary: "border-2",
-                text: "",
-            },
-            color: {
-                blue: "",
-                yellow: "",
-                purple: "",
-                dark: "",
-                light: "",
-            },
-            isJustified:{
-                true: "w-full"
-            }
+    base: "flex w-fit items-center justify-center rounded-full px-6 py-2 font-bold transition duration-200 hover:cursor-pointer",
+    variants: {
+        variant: {
+            primary: "border-2",
+            secondary: "",
+            tertiary: "border-2",
+            text: "",
         },
-        defaultVariants: {
+        color: {
+            blue: "",
+            yellow: "",
+            purple: "",
+            dark: "",
+            light: "",
+        },
+        isJustified: {
+            true: "w-full",
+        },
+    },
+    defaultVariants: {
+        variant: "primary",
+        color: "blue",
+    },
+    compoundVariants: [
+        {
             variant: "primary",
             color: "blue",
+            class: "border-blue-600 bg-blue-500 text-white hover:bg-blue-400",
         },
-        compoundVariants: [
-            {
-                variant: "primary",
-                color: "blue",
-                class: "bg-blue-500 text-white border-blue-600 hover:bg-blue-400"
-            },
-            {
-                variant: "primary",
-                color: "yellow",
-                class: "bg-yellow-brand text-gray-800 border-yellow-400 hover:bg-yellow-200",
-            },
-            {
-                variant: 'primary',
-                color: 'purple',
-                class: 'bg-purple-500 border-purple-600 text-white hover:bg-purple-400',
-            },
-            {
-                variant: 'primary',
-                color: 'dark',
-                class: 'bg-gray-800 border-gray-600 hover:bg-gray-700 text-white',
-            },
-            {
-                variant: 'primary',
-                color: 'light',
-                class: 'bg-gray-10 border-gray-200 text-gray-800 hover:bg-gray-100',
-            },
-            {
-                variant: 'secondary',
-                color: 'blue',
-                class: 'bg-blue-100 text-blue-750 hover:bg-blue-150',
-            },
-            {
-                variant: 'secondary',
-                color: 'yellow',
-                class: 'bg-yellow-100 text-yellow-750 hover:bg-yellow-150',
-            },
-            {
-                variant: 'secondary',
-                color: 'purple',
-                class: 'bg-purple-100 text-purple-750 hover:bg-purple-150',
-            },
-            {
-                variant: 'secondary',
-                color: 'dark',
-                class: 'bg-[#2E2B3B33] text-gray-800 hover:bg-[#2E2B3B26]',
-            },
-            {
-                variant: 'secondary',
-                color: 'light',
-                class: 'bg-[#C8C6D24D] text-white hover:bg-[#C8C6D233]',
-            },
-            {
-                variant: 'tertiary',
-                color: 'blue',
-                class: 'border-blue-200 text-blue-750 hover:bg-blue-50',
-            },
-            {
-                variant: 'tertiary',
-                color: 'yellow',
-                class: 'border-yellow-200 text-yellow-750 hover:bg-yellow-50',
-            },
-            {
-                variant: 'tertiary',
-                color: 'purple',
-                class: 'border-purple-200 text-purple-750 hover:bg-purple-50',
-            },
-            {
-                variant: 'tertiary',
-                color: 'dark',
-                class: 'text-gray-800 border-gray-800 border-opacity-20 hover:bg-[#2E2B3B0D]',
-            },
-            {
-                variant: 'tertiary',
-                color: 'light',
-                class: 'text-white border-gray-600 border-opacity-30 hover:bg-[#C8C6D20D]',
-            },
-            {
-                variant: 'text',
-                color: 'blue',
-                class: 'text-blue-750 hover:underline',
-            },
-            {
-                variant: 'text',
-                color: 'yellow',
-                class: 'text-yellow-750  hover:underline',
-            },
-            {
-                variant: 'text',
-                color: 'purple',
-                class: 'text-purple-750 hover:underline',
-            },
-            {
-                variant: 'text',
-                color: 'dark',
-                class: 'text-gray-800 hover:underline',
-            },
-            {
-                variant: 'text',
-                color: 'light',
-                class: 'text-white hover:underline',
-            }
-        ]
-    }
-);
+        {
+            variant: "primary",
+            color: "yellow",
+            class: "border-yellow-400 bg-yellow-brand text-gray-800 hover:bg-yellow-200",
+        },
+        {
+            variant: "primary",
+            color: "purple",
+            class: "border-purple-600 bg-purple-500 text-white hover:bg-purple-400",
+        },
+        {
+            variant: "primary",
+            color: "dark",
+            class: "border-gray-600 bg-gray-800 text-white hover:bg-gray-700",
+        },
+        {
+            variant: "primary",
+            color: "light",
+            class: "border-gray-200 bg-gray-10 text-gray-800 hover:bg-gray-100",
+        },
+        {
+            variant: "secondary",
+            color: "blue",
+            class: "bg-blue-100 text-blue-750 hover:bg-blue-150",
+        },
+        {
+            variant: "secondary",
+            color: "yellow",
+            class: "bg-yellow-100 text-yellow-750 hover:bg-yellow-150",
+        },
+        {
+            variant: "secondary",
+            color: "purple",
+            class: "bg-purple-100 text-purple-750 hover:bg-purple-150",
+        },
+        {
+            variant: "secondary",
+            color: "dark",
+            class: "bg-[#2E2B3B33] text-gray-800 hover:bg-[#2E2B3B26]",
+        },
+        {
+            variant: "secondary",
+            color: "light",
+            class: "bg-[#C8C6D24D] text-white hover:bg-[#C8C6D233]",
+        },
+        {
+            variant: "tertiary",
+            color: "blue",
+            class: "border-blue-200 text-blue-750 hover:bg-blue-50",
+        },
+        {
+            variant: "tertiary",
+            color: "yellow",
+            class: "border-yellow-200 text-yellow-750 hover:bg-yellow-50",
+        },
+        {
+            variant: "tertiary",
+            color: "purple",
+            class: "border-purple-200 text-purple-750 hover:bg-purple-50",
+        },
+        {
+            variant: "tertiary",
+            color: "dark",
+            class: "border-gray-800 border-opacity-20 text-gray-800 hover:bg-[#2E2B3B0D]",
+        },
+        {
+            variant: "tertiary",
+            color: "light",
+            class: "border-gray-600 border-opacity-30 text-white hover:bg-[#C8C6D20D]",
+        },
+        {
+            variant: "text",
+            color: "blue",
+            class: "text-blue-750 hover:underline",
+        },
+        {
+            variant: "text",
+            color: "yellow",
+            class: "text-yellow-750 hover:underline",
+        },
+        {
+            variant: "text",
+            color: "purple",
+            class: "text-purple-750 hover:underline",
+        },
+        {
+            variant: "text",
+            color: "dark",
+            class: "text-gray-800 hover:underline",
+        },
+        {
+            variant: "text",
+            color: "light",
+            class: "text-white hover:underline",
+        },
+    ],
+});
 
-interface ButtonProps extends VariantProps<typeof button>, Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'color'> {
+type CommonProps = {
     children: ReactNode;
-    href?: string;
+};
+
+type ButtonProps = VariantProps<typeof button> &
+    ButtonHTMLAttributes<HTMLButtonElement> &
+    CommonProps & { href?: never };
+type LinkProps = VariantProps<typeof button> & LinkHTMLAttributes<HTMLLinkElement> & CommonProps & { href: string };
+
+// Type guard to determine if the props are for a Link or Button.
+function isLinkProps(props: ButtonProps | LinkProps): props is LinkProps {
+    return props.href !== undefined;
 }
 
 /**
@@ -153,16 +160,19 @@ interface ButtonProps extends VariantProps<typeof button>, Omit<ButtonHTMLAttrib
  * Labels are provided as children of this element.
  * This Button dynamically determines its element type as either <button/> or <Link/> depending on the optional href attribute.
  */
-function Button({ children, href, ...props }: ButtonProps) {
+function Button(props: ButtonProps | LinkProps) {
     // Conditionally render as Link or button depending on whether a local link (href attribute) is provided.
-
-    if (href) {
+    if (isLinkProps(props)) {
+        // Is Link
+        const { href, children } = props;
         return (
             <Link href={href} className={button(props)}>
                 {children}
             </Link>
         );
     } else {
+        // Is button
+        const { children } = props;
         return (
             <button {...props} className={button(props)}>
                 {children}
