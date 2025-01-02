b0302d7093ea1a2a3580d8e2c98a2fd3094ef243
Tulza <Tulza0410@gmail.com>
2025-01-02 23:17:49 +1300

    💄 give initial random rotation to triangle

--- src/app/(home)/_components/BackgroundEffect.tsx
+++ src/app/(home)/_components/BackgroundEffect.tsx
@@ -2,7 +2,7 @@
 
 import React, { useRef } from "react";
 import { useFrame, Vector3 } from "@react-three/fiber";
-import { DoubleSide, Group, Mesh } from "three";
+import { DoubleSide, Euler, Group, Mesh } from "three";
 
 import { CommonLight } from "../../../components/three/CommonLight";
 import { TrianglePlane } from "../../../components/three/primitives/TrianglePlane";
@@ -10,14 +10,22 @@ import { TrianglePlane } from "../../../components/three/primitives/TrianglePlan
 // instance settings
 const RADIUS = 32;
 const COUNT = 64;
-const RAD_INCREMENT = (Math.PI * 2) / COUNT;
+const RAD_INCREMENT = (Math.PI * 2) / COUNT; // to space triangles evenly
 
 // movement settings
 const SPEED = Math.PI / 16; // ref: pi*2 is one full rotation per second
 const ROTCHAOS = 2;
 const POSCHAOS = 8;
 
-const TRIANGLEOBJ = ({ position, faceRotation }: { position: Vector3; faceRotation: number }) => {
+const TRIANGLEOBJ = ({
+    position,
+    rotation,
+    faceRotation,
+}: {
+    position: Vector3;
+    rotation: Euler;
+    faceRotation: number;
+}) => {
     const meshRef = useRef<Mesh>(null!);
     // if this forced re-render isn't here the background straight up doesn't load for some reason :skull:,
     // i've tried everything else
@@ -38,14 +46,8 @@ const TRIANGLEOBJ = ({ position, faceRotation }: { position: Vector3; faceRotati
     });
 
     return (
-        <group
-            position={position}
-            rotation={[0, faceRotation, 0]}
-            // initial={{ rotateZ: 0 }}
-            // animate={{ rotateZ: Math.PI * 2 }}
-            // transition={{ repeat: Infinity, duration: 8, ease: "linear" }}
-        >
-            <mesh ref={meshRef}>
+        <group position={position} rotation={[0, faceRotation, 0]}>
+            <mesh ref={meshRef} rotation={rotation}>
                 <TrianglePlane />
                 <meshBasicMaterial color="hsl(217, 100%, 61%)" side={DoubleSide} />
             </mesh>
@@ -54,14 +56,19 @@ const TRIANGLEOBJ = ({ position, faceRotation }: { position: Vector3; faceRotati
 };
 
 const TRIANGLES = [...Array(COUNT)].map((_, i) => {
+    // calculate initial values here
     const x = Math.cos(RAD_INCREMENT * i) * RADIUS + (Math.random() - 0.5) * POSCHAOS;
     const z = Math.sin(RAD_INCREMENT * i) * RADIUS;
     const y = (Math.random() - 0.5) * POSCHAOS;
 
+    const initrotx = 2 * Math.PI * Math.random();
+    const initroty = 2 * Math.PI * Math.random();
+
     const position = [x, y, z] as Vector3;
+    const rotation = new Euler(initrotx, initroty, 0, "XYZ");
     const faceRotation = -i * RAD_INCREMENT - Math.PI / 2;
-    // console.log(position);
-    return <TRIANGLEOBJ key={i} position={position} faceRotation={faceRotation} />;
+
+    return <TRIANGLEOBJ key={i} position={position} rotation={rotation} faceRotation={faceRotation} />;
 });
 
 const BackgroundEffect = () => {
