5783b3d26e8a9f807f985521d05a7dc2f0522f30
Kot <89110272+Kot6603@users.noreply.github.com>
2025-01-03 13:24:05 +1300

    integration(events): integrated header component into events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,14 +1,19 @@
 import StandardPageLayout from "@/components/StandardPageLayout";
 
-import EventsPageHeader from "./_components/EventsPageHeader";
 import EventsRoadmap from "./_components/EventsRoadmap";
 import UpcomingEventsSection from "./_components/UpcomingEventsSection";
 import { eventsData } from "./_data/events.data";
+import Header from "@/components/layout/Headers/Header";
 
 export default function EventsPage() {
     return (
         <StandardPageLayout>
-            <EventsPageHeader />
+            <Header variant="primary"
+                color="blue"
+                title="events"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis condimentum orci et congue. Donec at sagittis sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                primaryButton={{ label: "Upcoming events ⮕", href: "" }}
+                secondaryButton={{ label: "View past events", href: "/events/all" }} />
             <EventsRoadmap />
             <UpcomingEventsSection upcomingEvents={eventsData} />
         </StandardPageLayout>
