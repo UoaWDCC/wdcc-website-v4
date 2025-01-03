aff18d06939f1f34bcca95c818e3df5ce3f2db69
Kot <89110272+Kot6603@users.noreply.github.com>
2025-01-03 13:23:43 +1300

    style(events): larger header text for roadmap

--- src/app/events/_components/EventsRoadmap.tsx
+++ src/app/events/_components/EventsRoadmap.tsx
@@ -1,9 +1,8 @@
-import SectionWithHeader from "@/components/SectionWithHeader";
-
 export default function EventsRoadmap() {
     return (
-        <SectionWithHeader headerText="2025 Event Roadmap">
+        <div className="px-36 py-20">
+            <h2 className="text-3xl font-bold py-10">Events Roadmap 2025</h2>
             <div className="center-content h-[40em] bg-slate-300 rounded-2xl">EventsRoadmap</div>
-        </SectionWithHeader>
+        </div>
     );
 }
