ffbe5cfc1d7ce5bd0d7afc017950f2624e937bdd
Nate Williamson <nwilliamson@atlassian.com>
2025-01-03 23:49:42 +1100

    :bricks: restructure footer as nested flexboxes (to enable px-2 on columns)

--- src/components/navigation/footer/Footer.tsx
+++ src/components/navigation/footer/Footer.tsx
@@ -1,9 +1,10 @@
+import { HTMLAttributes } from "react";
+
 import { footerData } from "@/components/navigation/footer/_data/footer.data";
 import { cn } from "@/libs/utils";
 
 import FooterColumn from "./FooterColumn";
 import FooterSocials from "./FooterSocials";
-import { HTMLAttributes } from "react";
 
 interface FooterProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
@@ -11,23 +12,20 @@ interface FooterProps extends HTMLAttributes<HTMLDivElement> {
 
 export default function Footer({ className, ...props }: FooterProps) {
     return (
-        <footer {...props} className={cn("flex flex-col items-center bg-blue-900 px-10 pt-4 pb-20", className)}>
-            {/* Mobile: 1 column
-                Tablet: 3 columns
-                Desktop: 3 col, snap to max 768px
-            */}
-            <div
-                className={cn(
-                    "grid w-full grid-cols-1 whitespace-nowrap text-white sm:grid-cols-3 lg:max-w-screen-md",
-                )}
-            >
-                {footerData.columns.map((data, i) => (
-                    <FooterColumn key={data.title} title={data.title} links={data.links} last={i === 2} />
-                ))}
-                {/* blue bar */}
-                <div className="h-1 w-full my-8 rounded-full bg-gradient-to-br from-[#1F86E6] to-[#6B74A1] sm:col-span-3" />
-                <FooterSocials socials={footerData.socials} className="sm:col-span-3" />
+        <footer {...props} className={cn("flex justify-center bg-blue-900 px-10 pb-20 pt-4 text-white", className)}>
+            <div className="w-full lg:max-w-screen-md whitespace-nowrap">
+                <div className="flex flex-col justify-between gap-4 px-2 sm:flex-row">
+                    {footerData.columns.map((data, i) => (
+                        <FooterColumn key={data.title} title={data.title} links={data.links} last={i === 2} />
+                    ))}
+                </div>
+                <FooterBar />
+                <FooterSocials socials={footerData.socials} />
             </div>
         </footer>
     );
+}
+
+function FooterBar() {
+    return <div className="my-8 h-1 w-full rounded-full bg-gradient-to-br from-[#1F86E6] to-[#6B74A1]" />;
 }
