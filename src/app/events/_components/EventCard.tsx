1e7db703a3077a8c88d75fd7cde70f5ef3bf5736
Kot <89110272+Kot6603@users.noreply.github.com>
2025-01-06 19:08:37 +1300

    fix(events): updated event card component to match types

--- src/app/events/_components/EventCard.tsx
+++ src/app/events/_components/EventCard.tsx
@@ -1,27 +1,18 @@
 import Image from 'next/image';
 import placeholder from '@public/600x400.png';
-import { EventCardType } from '@/types/models';
+import { EventCardType } from '../_data/eventTypes';
 
 interface EventCardProps {
     event: EventCardType;
 }
 
-export default function EventCard({ event }: EventCardProps) {
-    let categoryColor: string;
-    switch (event.category) {
-        case "Workshop":
-            categoryColor = "bg-blue-200";
-            break;
-        case "Competition":
-            categoryColor = "bg-yellow-200";
-            break;
-        case "Social":
-            categoryColor = "bg-purple-200";
-            break;
-        default:
-            categoryColor = "bg-blue-200";
-    }
+const categoryColors = {
+    Workshop: "bg-blue-200",
+    Competition: "bg-yellow-200",
+    Social: "bg-purple-200",
+}
 
+export default function EventCard({ event }: EventCardProps) {
     return (
         <div className="h-[36rem] bg-slate-200 rounded-2xl flex-col justify-start items-end gap-9 inline-flex overflow-hidden">
             <div className="">
@@ -29,14 +20,14 @@ export default function EventCard({ event }: EventCardProps) {
             </div>
             <div className="self-stretch px-8 pb-8 flex-col justify-start items-center gap-6 flex">
                 <div className="justify-start items-start gap-2 inline-flex">
-                    <div className={`px-[18px] py-1 ${categoryColor} rounded-2xl justify-center items-center gap-2.5 overflow-hidden`}>
+                    <div className={`px-[18px] py-1 ${categoryColors[event.category]} rounded-2xl justify-center items-center gap-2.5 overflow-hidden`}>
                         <p className="text-black text-sm font-bold">{event.category}</p>
                     </div>
                 </div>
                 <div className="self-stretch flex-col justify-start items-start gap-3 flex">
                     <p className="self-stretch text-center text-black text-xl font-bold leading-[25.20px]">{event.title}</p>
                     <div className="self-stretch h-[38px] flex-col justify-start items-start flex">
-                        <p className="self-stretch text-center text-[#1453b7] font-bold leading-tight">{event.time}</p>
+                        <p className="self-stretch text-center text-[#1453b7] font-bold leading-tight" suppressHydrationWarning>{event.time.toLocaleString()}</p>
                         <p className="self-stretch text-center text-[#1453b7] font-medium italic leading-tight">{event.location}</p>
                     </div>
                     <p className="self-stretch text-center font-medium leading-tight">{event.description}</p>
