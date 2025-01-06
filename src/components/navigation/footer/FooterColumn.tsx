e0274000727d3c90b45ed2408550235b6c0685bc
Oculux <121474948+Oculux314@users.noreply.github.com>
2025-01-07 03:21:42 +1100

    :lipstick: color footer headers blue on mobile
    Co-authored-by: Tulza <40633228+tulza@users.noreply.github.com>

--- src/components/navigation/footer/FooterColumn.tsx
+++ src/components/navigation/footer/FooterColumn.tsx
@@ -6,7 +6,7 @@ import type { FooterColumn } from "./_data/footerTypes";
 export default function FooterColumn({ title, links }: FooterColumn) {
     return (
         <div className="flex select-none flex-col">
-            <h3 className="mb-4 border-b border-white/70 py-4 text-lg font-bold text-yellow-brand sm:mb-1 sm:border-none sm:pb-0 sm:text-blue-150">
+            <h3 className="mb-4 border-b border-white/70 py-4 text-lg font-bold sm:mb-1 sm:border-none sm:pb-0 text-blue-150">
                 {title}
             </h3>
             <div className={cn("flex flex-col")}>
