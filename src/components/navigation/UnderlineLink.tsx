45481aa76c73e5d5e973ce8216fba80fad84d932
Nate Williamson <nwilliamson@atlassian.com>
2025-01-07 03:26:34 +1100

    :bug: ensure underline links in footer animate in

--- src/components/navigation/UnderlineLink.tsx
+++ src/components/navigation/UnderlineLink.tsx
@@ -19,8 +19,8 @@ export const UnderlineLink = ({ children, newTab, mode = "dark", shift, ...props
                 <p className={cn(shift && "transition-transform group-hover:translate-x-1")}>{children}</p>
                 <div
                     className={cn(
-                        "pointer-events-none h-0.5 w-0 rounded transition-[width] group-hover:w-1/2",
-                        shift && "transition-transform group-hover:translate-x-1",
+                        "pointer-events-none h-0.5 w-0 rounded transition-[width,transform] group-hover:w-1/2",
+                        shift && "group-hover:translate-x-1",
                         mode === "light" ? "bg-blue-100" : "bg-gray-700"
                     )}
                 />
