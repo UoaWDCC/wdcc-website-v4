f7f785d0878af7541b2e9ce3960e6e1b42b09df7
Nate Williamson <nwilliamson@atlassian.com>
2025-01-10 09:47:35 +1100

    :sparkles: add responsive body style and apply it to standard page layout

--- src/components/StandardPageLayout.tsx
+++ src/components/StandardPageLayout.tsx
@@ -12,13 +12,13 @@ interface StandardPageLayoutProps extends HtmlHTMLAttributes<HTMLElement> {
 /** This component replaces a typical layout.tsx - allowing us to benefit from a consistent layout, but also optionally disable it. */
 export default function StandardPageLayout({ children, ...props }: StandardPageLayoutProps) {
     return (
-        <div {...props} className={cn("flex min-h-full flex-col", props.className)}>
+        <div {...props} className={cn("flex min-h-dvh flex-col", props.className)}>
             <div className="relative flex h-dvh min-h-dvh flex-col overflow-x-hidden">
                 <div className="sticky top-0 z-10">
                     <NavigationBar />
                 </div>
-                <main className="relative flex flex-col justify-between">{children}</main>
-                {/* Combination of min-h-full and mt-auto ensures footer doesn't collapse above screen bottom on small pages. */}
+                <main className="responsive-body">{children}</main>
+                {/* Combination of min-h-dvh and mt-auto ensures footer doesn't collapse above screen bottom on small pages. */}
                 <Footer className="mt-auto" />
             </div>
         </div>
