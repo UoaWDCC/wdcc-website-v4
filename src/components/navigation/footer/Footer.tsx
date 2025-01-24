35de831e12cdb273e622631428dcdb001663d360
Andrew Qiu <git@andrewqiu.com>
2025-01-24 18:32:29 +1300

    ✨ styling corrections to footer

--- src/components/navigation/footer/Footer.tsx
+++ src/components/navigation/footer/Footer.tsx
@@ -12,8 +12,8 @@ interface FooterProps extends HTMLAttributes<HTMLDivElement> {
 
 export default function Footer({ className, ...props }: FooterProps) {
     return (
-        <footer {...props} className={cn("flex justify-center bg-blue-900 pb-20 pt-4 text-white xs:px-10", className)}>
-            <div className="w-full whitespace-nowrap lg:max-w-screen-md">
+        <footer {...props} className={cn("flex justify-center bg-blue-900 pt-12 pb-16 text-white xs:px-10", className)}>
+            <div className="w-full whitespace-nowrap lg:max-w-screen-md flex flex-col gap-8">
                 <div className="hidden-scrollbar flex flex-col justify-between gap-4 overflow-x-auto px-4 sm:flex-row">
                     {footerData.columns.map((data) => (
                         <FooterColumn key={data.title} title={data.title} links={data.links} />
@@ -27,5 +27,5 @@ export default function Footer({ className, ...props }: FooterProps) {
 }
 
 function FooterBar() {
-    return <div className="my-8 h-[3px] w-full rounded-full bg-gradient-to-tr from-blue-300 to-blue-500" />;
+    return <div className="h-[3px] w-full rounded-full bg-gradient-to-tr from-blue-300 to-blue-500" />;
 }
