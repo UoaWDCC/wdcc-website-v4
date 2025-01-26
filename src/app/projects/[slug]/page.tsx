1724f9bb06a02d388ba983a128d416604dbbe688
Tulza <Tulza0410@gmail.com>
2025-01-26 13:04:29 +1300

    🔍️ added basic seo

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata, ResolvingMetadata } from "next";
 import { notFound } from "next/navigation";
 
 import RouteDisplay from "@/components/debug/RouteDisplay";
@@ -6,6 +7,30 @@ import StandardPageLayout from "@/components/layout/StandardPageLayout";
 
 import { projectsData } from "../_data/projects_data/index";
 import IndividualProject from "./_component/IndividualProject";
+
+type Props = {
+    params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+    const slug = (await params).slug;
+
+    const project = projectsData.find((p) => p.slug === `/${slug}`);
+
+    const previousImages = (await parent).openGraph?.images || [];
+
+    if (!project) {
+        return {};
+    }
+
+    return {
+        title: project.projectName,
+        description: project.description,
+        openGraph: {
+            images: [...previousImages],
+        },
+    };
+}
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
     const slug = (await params).slug;
