66a291fa3524f67b674bbe0a2644e039b92b12c6
Tulza <Tulza0410@gmail.com>
2025-01-27 14:18:45 +1300

    🩹 go to not found if event doesn't exist

--- src/app/events/[slug]/_component/IndividualEvent.tsx
+++ src/app/events/[slug]/_component/IndividualEvent.tsx
@@ -1,23 +1,26 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
 import ImageFit from "@/components/ImageFit";
+
 import { eventsData } from "../_data/individualEvent.data";
 import { IndividualEventType } from "../../_data/eventTypes";
 
 const IndividualEvent = ({ title }: { title: string }) => {
-    const event: IndividualEventType = eventsData[title]
-    console.log(event)
+    const event: IndividualEventType = eventsData[title];
+
+    if (!event) {
+        return notFound();
+    }
 
     return (
         <div className="flex flex-col py-16">
             <div className="grid grid-cols-2 gap-16">
                 {/* todo: replace with real data soon. payload? */}
-                <p className="whitespace-pre-line text-md">
-                    {event.description}
-                </p>
+                <p className="whitespace-pre-line text-md">{event.description}</p>
                 <ImageFit src={event.image} alt="" height="500px" width="500px" />
             </div>
-            {event.collabPartners.length > 0 &&
+            {event.collabPartners.length > 0 && (
                 <div className="mt-8">
                     {/* todo: add an 's' to the end if multiple */}
                     <h3 className="text-3xl font-semibold">Collab Partners</h3>
@@ -27,7 +30,7 @@ const IndividualEvent = ({ title }: { title: string }) => {
                         ))}
                     </div>
                 </div>
-            }
+            )}
         </div>
     );
 };
