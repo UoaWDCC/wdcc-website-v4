88428c2b226143d6bf05327cbd047799d19fd7fe
Tulza <Tulza0410@gmail.com>
2025-02-08 00:36:30 +1300

    :arrow_up: upgrade to next15

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
+import type { NextConfig } from "next";
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const bundleAnalyzer = withBundleAnalyzer({
     enabled: process.env.ANALYZE === "true",
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
+    experimental: {
+        // reactCompiler: true,
+    },
     compiler: {
         removeConsole: process.env.NODE_ENV === "production",
     },
