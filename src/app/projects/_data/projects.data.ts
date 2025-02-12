344f5cc83be91b89ad2ea1f67369bc7b8b4be561
Andrew Qiu <git@andrewqiu.com>
2025-02-13 03:13:54 +1300

    🐛 typescripting

--- src/app/projects/_data/projects.data.ts
+++ src/app/projects/_data/projects.data.ts
@@ -29,25 +29,25 @@ export const projectsCopy = {
         title: "Project Roles",
         roles: [
             {
-                color: "blue",
+                color: "blue" as const,
                 title: "Developer",
                 slug: "developer",
                 description: "WDCC Developers are the heart of every project team. You could work on anything - from adding a brand-new CMS through to keyframing in CSS.",
             },
             {
-                color: "purple",
+                color: "purple" as const,
                 title: "Designer",
                 slug: "designer",
                 description: "WDCC Designers are a step beyond theory. You'll navigate genuine design problems with a real-world project backed by a large technical team.",
             },
             {
-                color: "yellow",
+                color: "yellow" as const,
                 title: "Tech Lead",
                 slug: "techlead",
                 description: "WDCC Tech Leads get the senior dev experience (without the clout). Direct a large student team, and make your CV shine along the way.",
             },
             {
-                color: "green",
+                color: "green" as const,
                 title: "Project Manager",
                 slug: "projectmanager",
                 description: "WDCC PMs get to handle communication and Agile workflows in industry-like ways, but helped by friends instead of managers.",
