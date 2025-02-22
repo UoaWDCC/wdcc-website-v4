a8b7282b008d35504498810f523617e9e3fb6d0e
Andrew Qiu <andrewqqiu@gmail.com>
2025-02-23 10:57:59 +1300

    Update enterprise.data.ts

--- src/app/(home)/brainrot-lol/pricing-xdx-real/_data/enterprise.data.ts
+++ src/app/(home)/brainrot-lol/pricing-xdx-real/_data/enterprise.data.ts
@@ -1,4 +1,4 @@
-import { ChecklistType } from "../_components/Checklist";
+import { ChecklistType } from "@/components/primitives/Checklist";
 
 export const enterpriseData: { checklist: ChecklistType[] } = {
     checklist: [
