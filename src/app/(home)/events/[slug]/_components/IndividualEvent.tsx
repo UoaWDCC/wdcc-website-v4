bb5a7bd9b96fef27b2b0cc60562592863a077677
Oculux314 <nate.williamson.nz@gmail.com>
2025-02-28 02:15:43 +1300

    :lipstick: make collab partners non-pluralised if only 1 partner

--- src/app/(home)/events/[slug]/_components/IndividualEvent.tsx
+++ src/app/(home)/events/[slug]/_components/IndividualEvent.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Link from "next/link";
 
 import { Event } from "@/types/models";
@@ -9,25 +8,28 @@ const IndividualEvent = ({ event }: { event: Event }) => {
     return (
         <div className="flex flex-col py-16">
             <div className="grid grid-cols-2 gap-16">
-                {/* todo: replace with real data soon. payload? */}
-                {/* done 🤝*/}
                 <p className="whitespace-pre-line text-md">{event.page.description}</p>
                 <ImageFit src={event.page.image.src} alt="" height="500px" width="500px" />
-                {event.partners && event.partners.length > 0 && (
-                    <div className="mt-8">
-                        {/* todo: add an 's' to the end if multiple */}
-                        <h3 className="text-3xl font-semibold">Collab Partners</h3>
-                        <div className="flex gap-4">
-                            {event.partners.map((partner, index) => (
-                                <Link href={partner.href} target="_" key={index}>
-                                    <ImageFit key={partner.alt} src={partner.src} alt="" height="100px" width="300px" />
-                                </Link>
-                            ))}
-                        </div>
-                    </div>
-                )}
+                <EventPartners partners={event.partners} />
             </div>
         </div>
+    );
+};
+
+const EventPartners = ({ partners }: { partners: Event["partners"] }) => {
+    return (partners.length > 0 && (
+            <div className="mt-8">
+                {/* Partner's' if multiple */}
+                <h3 className="text-3xl font-semibold">Collab Partner{partners.length > 1 && "s"}</h3>
+                <div className="flex gap-4">
+                    {partners.map((partner, index) => (
+                        <Link href={partner.href} target="_" key={index}>
+                            <ImageFit key={partner.alt} src={partner.src} alt="" height="100px" width="300px" />
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        )
     );
 };
 
