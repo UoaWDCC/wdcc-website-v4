10a47fdad3e2c93f86af7c040f2c6792ffa58d88
Tulza <Tulza0410@gmail.com>
2025-02-27 21:32:13 +1300

    :lipstick: mini pill sponsor

--- src/app/(home)/_components/SponsorSection.tsx
+++ src/app/(home)/_components/SponsorSection.tsx
@@ -1,23 +1,58 @@
-import React from "react";
+import React, { HTMLAttributes } from "react";
+import { tv } from "tailwind-variants";
 
 import { sponsors } from "@/assets/image/sponsors";
+import { cn } from "@/libs/utils";
 
 import SponsorCard from "./sponsor/SponsorCard";
 
 export const SponsorSection = () => {
     return (
-        <div className="flex flex-col items-center justify-center gap-12">
+        <div className="flex flex-col items-center justify-center gap-10">
             <h2 className="text-balance text-3xl font-bold leading-none">Our sponsors for 2025</h2>
-            <div className="flex flex-col items-center justify-center gap-6">
+            <div className="flex w-full flex-col items-center justify-center">
                 {/* horror */}
                 {(Object.keys(sponsors) as Array<keyof typeof sponsors>).map((tier) => (
-                    <div className="flex flex-wrap justify-center gap-4" key={tier}>
-                        {sponsors[tier].map((logo, i) => (
-                            <SponsorCard tier={tier} src={logo} alt={logo} width="150px" key={i} />
-                        ))}
-                    </div>
+                    <>
+                        <SponsorLabel tier={tier} className="mt-8" />
+                        <div
+                            className="responsive-grid mt-6 grid w-full flex-col place-content-center place-items-center gap-2 md:gap-4"
+                            key={tier}
+                        >
+                            {sponsors[tier].map((logo, i) => (
+                                <SponsorCard tier={tier} src={logo} alt={logo} width="150px" height="100px" key={i} />
+                            ))}
+                        </div>
+                    </>
                 ))}
             </div>
+        </div>
+    );
+};
+
+const sponsorLabel = tv({
+    base: "whitespace-nowrap rounded-full border-2 px-4 font-semibold",
+    variants: {
+        tier: {
+            gold: "border-yellow-400 bg-sponsor-gold",
+            tech: "border-purple-200 bg-sponsor-tech",
+            silver: "border-gray-200 bg-sponsor-silver",
+            community: "border-green-200 bg-sponsor-community",
+        },
+    },
+    defaultVariants: {
+        tier: "gold",
+    },
+});
+
+interface SponsorLabelProps extends HTMLAttributes<HTMLDivElement> {
+    tier: keyof typeof sponsors;
+}
+
+const SponsorLabel = ({ tier, ...props }: SponsorLabelProps) => {
+    return (
+        <div {...props} className={cn(sponsorLabel({ tier: tier }), props.className)}>
+            {tier} sponsor
         </div>
     );
 };
--- src/app/(home)/_components/sponsor/SponsorCard.tsx
+++ src/app/(home)/_components/sponsor/SponsorCard.tsx
@@ -27,8 +27,8 @@ const line = tv({
 });
 
 const tierWidth: Record<Exclude<TierKeys, undefined>, number> = {
-    gold: 200,
-    tech: 200,
+    gold: 250,
+    tech: 250,
     silver: 150,
     community: 150,
 };
@@ -40,38 +40,12 @@ interface SponsorCardProps extends ImageFitProps {
 }
 const SponsorCard = ({ tier, ...image }: SponsorCardProps) => {
     return (
-        <motion.div
-            initial="hide"
-            whileHover="show"
-            className="relative cursor-pointer overflow-hidden bg-white p-4 px-6 shadow-lg"
-        >
-            <div className={line({ position: "top", tier: tier })} />
-            <div className={line({ position: "bottom", tier: tier })} />
-            <motion.p
-                variants={textvariants}
-                className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 text-center font-bold drop-shadow-[0px_0px_4px_white]"
-                transition={{ duration: 1, ease: easeOutExpo }}
-            >
-                Sponsor
-            </motion.p>
-            <motion.div variants={imagevariants} transition={{ duration: 1, ease: easeOutExpo }}>
-                {/* wow this is awful, might be the worst code i've ever written 😭 oh well it's 2 a.m. */}
-                <ImageFit {...image} width={`${tierWidth[tier || "gold"]}px`} />
-            </motion.div>
-        </motion.div>
+        <ImageFit
+            {...image}
+            width={`${tierWidth[tier || "gold"]}px`}
+            className="drop-shadow-[0px_0px_8px_var(--background)]"
+        />
     );
-};
-
-// experimental
-
-const textvariants: Variants = {
-    hide: { opacity: 0 },
-    show: { opacity: 1 },
-};
-
-const imagevariants: Variants = {
-    hide: { scale: 1, filter: "blur(0px)" },
-    show: { scale: 0.8, filter: "blur(4px)" },
 };
 
 export default SponsorCard;
