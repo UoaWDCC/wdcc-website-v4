935c68dfad40e02606c11dce0781d9b5e1cd5907
Andrew Qiu <git@andrewqiu.com>
2025-02-28 13:14:17 +1300

    Update EventsRoadmap.tsx

--- src/app/(home)/events/_components/EventsRoadmap.tsx
+++ src/app/(home)/events/_components/EventsRoadmap.tsx
@@ -10,7 +10,7 @@ interface EventsRoadmapProps {
 
 export default function EventsRoadmap({ title, image }: EventsRoadmapProps) {
     return (
-        <div className="py-20">
+        <div className="py-20" id="roadmap">
             <h2 className="py-10 text-3xl font-bold">{title}</h2>
             <Image className="w-full rounded-2xl" src={image.src} alt={image.alt} width="4000" height="3000"/>
         </div>
