ca98aa536f85b38d11585cf0b6c35f21ae5cc589
Tulza <Tulza0410@gmail.com>
2025-03-01 20:08:20 +1300

    :sparkles: added faq select

--- src/app/(home)/about/faq/_components/FaqSelector.tsx
+++ src/app/(home)/about/faq/_components/FaqSelector.tsx
@@ -14,31 +14,24 @@ interface FaqSelectorProps {
 }
 
 export function FaqSelector({ sections }: FaqSelectorProps) {
-    const [selectedTab, setSelectedTab] = useState({ name: sections[0].name, faqs: sections[0].faqs });
-    const [selectedFaq, setSelectedFaq] = useState({});
+    const [selectedTab, setSelectedTab] = useState(sections[0]);
+    const [selectedFaq, setSelectedFaq] = useState<Faq | null>(null);
 
-    const handleTabSelect = (tab: string) => () => {
-        setSelectedTab({ name: tab, faqs: sections.find((section) => section.name === tab)?.faqs || [] });
+    const handleTabSelect = (tabName: string) => {
+        const foundSection = sections.find((section) => section.name === tabName);
+        if (foundSection) {
+            setSelectedTab(foundSection);
+            setSelectedFaq(null); // Reset selected FAQ when changing tabs
+        }
     };
 
-    const handleItemSelect = (faq: Faq) => () => {
-        setSelectedFaq(faq === selectedFaq ? {} : faq);
+    const handleItemSelect = (faq: Faq) => {
+        setSelectedFaq(selectedFaq?.question === faq.question ? null : faq);
     };
 
     return (
         <div className="flex w-full flex-col items-start justify-between gap-4 self-stretch md:flex-row">
-            <div className="flex w-[400px] flex-col items-start justify-center gap-2">
-                {sections.map((section) => (
-                    <FaqTab
-                        key={section.name}
-                        colors={section.colors}
-                        text={section.name}
-                        selected={selectedTab.name === section.name}
-                        handleClick={handleTabSelect(section.name)}
-                    />
-                ))}
-            </div>
-
+            <FaqCategorySelect sections={sections} selectedTab={selectedTab} handleTabSelect={handleTabSelect} />
             <motion.div
                 key={selectedTab.name}
                 variants={itemContainerVariants}
@@ -51,14 +44,78 @@ export function FaqSelector({ sections }: FaqSelectorProps) {
                     <FaqItem
                         key={faq.question}
                         faq={faq}
-                        selected={selectedFaq === faq}
-                        handleClick={handleItemSelect(faq)}
+                        selected={selectedFaq?.question === faq.question}
+                        handleClick={() => handleItemSelect(faq)}
                     />
                 ))}
             </motion.div>
         </div>
     );
 }
+
+interface FaqCategorySelectorProps {
+    sections: FaqSection[];
+    selectedTab: FaqSection;
+    handleTabSelect: (tabName: string) => void;
+}
+
+const FaqCategorySelect = ({ sections, handleTabSelect, selectedTab }: FaqCategorySelectorProps) => {
+    return (
+        <>
+            <div className="flex w-full justify-end md:hidden">
+                <FaqSelect sections={sections} handleTabSelect={handleTabSelect} />
+            </div>
+            <div className="hidden w-[400px] flex-col items-start justify-center gap-2 md:flex">
+                {sections.map((section) => (
+                    <FaqTab
+                        key={section.name}
+                        colors={section.colors}
+                        text={section.name}
+                        selected={selectedTab.name === section.name}
+                        handleClick={() => handleTabSelect(section.name)}
+                    />
+                ))}
+            </div>
+        </>
+    );
+};
+
+interface FaqSelectProps {
+    sections: FaqSection[];
+    handleTabSelect: (section: string) => void;
+}
+
+const FaqSelect = ({ sections, handleTabSelect }: FaqSelectProps) => {
+    const [selected, setSelected] = useState(sections[0]);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleSelect = (section: FaqSection) => {
+        setSelected(section);
+        handleTabSelect(section.name);
+        setIsOpen(false);
+    };
+
+    return (
+        <div className="relative">
+            <button onClick={() => setIsOpen(!isOpen)} className="rounded-full bg-gray-200 p-0.5 px-4 font-semibold">
+                {selected.name}
+            </button>
+            {isOpen && (
+                <div className="absolute right-0 top-full translate-y-1 flex-col gap-1 rounded-xl bg-gray-200 p-1">
+                    {sections.map((section) => (
+                        <button
+                            key={section.name}
+                            onClick={() => handleSelect(section)}
+                            className="w-full cursor-pointer rounded-lg px-4 text-right hover:bg-black"
+                        >
+                            {section.name}
+                        </button>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
 
 const itemContainerVariants = {
     open: {
