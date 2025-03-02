181a1df223b6d60cdc2c237ddb5a795a5ce7f588
Oculux314 <nate.williamson.nz@gmail.com>
2025-03-03 06:05:19 +1300

    :bug: :rocket: alter deployment scripts to connect to staging database at build time and allow workflow_dispatch for prod builds

--- src/app/(home)/admin/revalidate/revalidate-action.ts
+++ src/app/(home)/admin/revalidate/revalidate-action.ts
@@ -4,4 +4,5 @@ import { revalidatePath } from "next/cache";
 
 export async function revalidateAllRoutes() {
     revalidatePath("/");
+    console.log("All routes revalidated at", new Date().toISOString());
 }
