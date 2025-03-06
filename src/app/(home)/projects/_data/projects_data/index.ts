a0206c74fe5b20ffb99fe1195843cad9fa98a7e6
Oculux314 <nate.williamson.nz@gmail.com>
2025-03-07 09:52:44 +1300

    :fire: remove projects data

--- src/app/(home)/projects/_data/projects_data/index.ts
+++
@@ -1,9 +0,0 @@
-import { Project } from "@/types/models";
-
-import { aspa2024 } from "./aspa-2024";
-import { nzpmc2023 } from "./nzpmc-2023";
-import { uabc2024 } from "./uabc-2024";
-import { uasc2024 } from "./uasc-2024";
-import { youthOfOrakei2024 } from "./youth-of-orakei-2024";
-
-export const projectsData: Project[] = [uabc2024, uasc2024, aspa2024, youthOfOrakei2024, nzpmc2023];
