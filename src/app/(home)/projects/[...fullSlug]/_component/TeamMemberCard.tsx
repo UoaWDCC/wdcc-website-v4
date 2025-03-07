20a644ed193651f38ae17e03d60e6f69250e589c
Oculux314 <nate.williamson.nz@gmail.com>
2025-03-07 22:49:37 +1300

    :sparkles: implement project team images with placeholder

--- src/app/(home)/projects/[...fullSlug]/_component/TeamMemberCard.tsx
+++ src/app/(home)/projects/[...fullSlug]/_component/TeamMemberCard.tsx
@@ -1,5 +1,10 @@
-import React from "react";
 import { tv, VariantProps } from "tailwind-variants";
+
+import { ImageType } from "@/types/image";
+
+import ImageCover from "@/components/ImageCover";
+
+import placeholder from "../_assets/placeholder.png";
 
 const card = tv({
     base: "flex w-min flex-col gap-4 rounded-lg p-4",
@@ -18,15 +23,18 @@ const card = tv({
 
 type Variant = { variant?: VariantProps<typeof card> };
 
-interface teamCardProps extends Variant {
+interface TeamCardProps extends Variant {
     name: string;
     role: string;
+    image?: ImageType;
 }
 
-const TeamMemberCard = ({ name, role, variant }: teamCardProps) => {
+const TeamMemberCard = ({ name, role, image, variant }: TeamCardProps) => {
     return (
         <div className={card(variant)}>
-            <div className="h-[150px] w-[200px] rounded-sm bg-gray-50"></div>
+            <div className="h-[150px] w-[200px] rounded-md bg-gray-50">
+                <ImageCover src={image?.src || placeholder} alt={image?.alt || name} height={"150px"} width="200px" />
+            </div>
             <div className="space-2">
                 <p className="font-semibold">{name}</p>
                 <p>{role}</p>
