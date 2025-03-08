5750491ff2e3d0c13c1e18cb41abfde64dd0ced7
Oculux314 <nate.williamson.nz@gmail.com>
2025-03-08 20:29:53 +1300

    :shirt: remove unused import

--- src/app/(home)/brainrot-lol/wine-drinking-champagne-club/page.tsx
+++ src/app/(home)/brainrot-lol/wine-drinking-champagne-club/page.tsx
@@ -3,10 +3,9 @@
 import { Cardo } from "next/font/google";
 import Image from "next/image";
 
-import tuscanyLandscape from "@/assets/brainrot/tuscany-landscape.jpg";
 import mainschleifeLandscape from "@/assets/brainrot/mainschleife-landscape.jpg";
+import tuscanyLandscape from "@/assets/brainrot/tuscany-landscape.jpg";
 import w1 from "@/assets/brainrot/w1.jpg";
-import w2 from "@/assets/brainrot/w2.jpg";
 import w3 from "@/assets/brainrot/w3.jpg";
 import StandardPageLayout from "@/components/layout/StandardPageLayout";
 
