aff89fd230d7366747545bda6020fbd3be0ab95c
Andrew Qiu <git@andrewqiu.com>
2025-03-10 01:34:31 +1300

    🚧 allow  role card to deal with wip pages

--- src/app/(home)/projects/_components/ProjectRoleCard.tsx
+++ src/app/(home)/projects/_components/ProjectRoleCard.tsx
@@ -11,7 +11,7 @@ import Image, {StaticImageData} from 'next/image';
 
 const card = tv({
     base: "relative w-full flex flex-col gap-6 overflow-hidden rounded-2xl sm:py-12 sm:px-11 py-9 px-9 " +
-        "transition will-change-auto duration-500 hover:duration-200 hover:-translate-x-1 hover:-translate-y-1 cursor-pointer hover:drop-shadow-cardLift",
+        "transition will-change-auto duration-500 hover:duration-200 hover:-translate-x-1 hover:-translate-y-1 hover:drop-shadow-cardLift",
     variants: {
         color: {
             blue: "bg-blue-100",
@@ -33,31 +33,57 @@ export interface ProjectRoleCardProps {
 
 const ProjectRoleCard = ({ variant, title, description, slug, graphic }: ProjectRoleCardProps) => {
     return (
-        <Link className={card({ ...variant })} href={"/projects/roles/" + slug}>
-            <div className="flex justify-between items-center">
-                <h3 className="text-2xl font-bold text-gray-800 leading-none">{title}</h3>
-                <Button variant={{ style: "tertiary", color: "dark"}} className="min-h-[39px]">
-                    <span className="hidden sm:inline"> Learn </span>
-                    <Arrow></Arrow>
-                </Button>
+        slug
+            ?
+            // Render typical card if there is a slug (i.e. the card links somewhere)
+            <Link className={card({ ...variant })} href={"/projects/roles/" + slug}>
+                <div className="flex justify-between items-center">
+                    <h3 className="text-2xl font-bold text-gray-800 leading-none">{title}</h3>
+                    <Button variant={{ style: "tertiary", color: "dark"}} className="min-h-[39px]">
+                        <span className="hidden sm:inline"> Learn </span>
+                        <Arrow></Arrow>
+                    </Button>
+                </div>
+
+                <p className="leading-tight text-md font-normal">
+                    {description}
+                </p>
+
+                {
+                    graphic ?
+                        <div className="w-full rounded-xl relative overflow-hidden">
+                            <Image src={graphic} alt="" height={1000}/>
+                        </div>
+                        :
+                        <div
+                            className="h-[60px] w-full rounded-xl bg-linear-to-r from-[#C9A9FF] to-[#FFA4A5] flex items-center justify-center">
+                            <p className="text-white">Coming soon!</p>
+                        </div>
+                }
+            </Link>
+            :
+            // Render non-clickable placeholder otherwise.
+            <div className={card({ ...variant })}>
+                <div className="flex justify-between items-center">
+                    <h3 className="text-2xl font-bold text-gray-800 leading-none">{title}</h3>
+                </div>
+
+                <p className="leading-tight text-md font-normal">
+                    {description}
+                </p>
+
+                {
+                    graphic ?
+                        <div className="w-full rounded-xl relative overflow-hidden">
+                            <Image src={graphic} alt="" height={1000}/>
+                        </div>
+                        :
+                        <div
+                            className="h-[60px] w-full rounded-xl bg-linear-to-r from-[#C9A9FF] to-[#FFA4A5] flex items-center justify-center">
+                            <p className="text-white">Coming soon!</p>
+                        </div>
+                }
             </div>
-
-            <p className="leading-tight text-md font-normal">
-                {description}
-            </p>
-
-            {
-                graphic ?
-                    <div className="w-full rounded-xl relative overflow-hidden">
-                        <Image src={graphic} alt="" height={1000}/>
-                    </div>
-                    :
-                    <div
-                        className="h-[60px] w-full rounded-xl bg-linear-to-r from-[#C9A9FF] to-[#FFA4A5] flex items-center justify-center">
-                        <p className="text-white">Coming soon!</p>
-                    </div>
-            }
-        </Link>
     );
 };
 
