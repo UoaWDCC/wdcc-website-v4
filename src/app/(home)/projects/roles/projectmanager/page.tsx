31ee2a5a1776f4546f393b8a82c8a60ed044565f
Andrew Qiu <git@andrewqiu.com>
2025-03-12 14:02:41 +1300

    ✨ init new pages with template

--- src/app/(home)/projects/roles/projectmanager/page.tsx
+++ src/app/(home)/projects/roles/projectmanager/page.tsx
@@ -1,23 +1,134 @@
 "use client";
 
-import Header from "@/components/layout/pageheaders/Header";
+import React from "react";
+
+import RolesHeader from "@app/projects/roles/_components/RolesHeader";
 import StandardPageLayout from "@/components/layout/StandardPageLayout";
 
+import { Button } from "@/components/primitives/Button";
+import SignupCard from "@app/_components/SignupCard";
+import LearnCard from "@app/projects/roles/_components/LearnCard";
+import RiveToggleButton from "@app/projects/roles/_components/RiveToggleButton";
+import RiveDuo from "@/components/rive/RiveDuo";
+import RiveWrapper from "@/components/rive/RiveWrapper";
+
+import Arrow from "@/assets/svg/Arrow"
+import MoreAboutProjects from "@app/projects/roles/_components/MoreAboutProjects";
+import PastProjects from "@app/projects/roles/_components/PastProjects";
+
+import graphic1 from "@/assets/image/roles/roles_design-01.webp";
+import graphic2 from "@/assets/image/roles/roles_design-02.webp";
+import graphic3 from "@/assets/image/roles/roles_design-03.webp";
+
+
 export default function PMPage() {
+
+    const [animEnabled, setAnimEnabled] = React.useState(true);
+
     return (
-        <StandardPageLayout>
-            <Header
-                variant={{ style: "secondary", color: "green" }}
-                title="make agile great again as a WDCC Project Manager"
+        // Yes I know it's orange. Contrast with yellow is bad so just deal with it.
+        <StandardPageLayout className="bg-orange-500" navColor="dark">
+
+            <RiveToggleButton animEnabled={animEnabled} setAnimEnabled={setAnimEnabled}/>
+
+            <RolesHeader
+                variant={{ color: "yellow" }}
+                title="Lead to succeed as a WDCC Project Manager"
+                role="Project Manager"
+                description1="The best web development projects don’t get there just because of code."
+                description2="As a designer in a WDCC Project, you’ll help craft beautiful, usable, and practical solutions to some of our community’s coolest tech challenges."
                 backlink={{ label: "projects", href: "/projects#roles" }}
             />
 
-            <div className="flex flex-col gap-24 py-24">
-                <div className="flex flex-col gap-8">
-                    <h2 className="text-3xl font-bold">content tba</h2>
-                    <p>content tba</p>
+            <div className="flex flex-col gap-42 pb-32 text-white responsive-fullwidth">
+                <div className="flex flex-col gap-2 justify-center items-center w-screen m-0">
+                    {/*<div className="h-[500px] rounded-2xl w-full bg-purple-300"></div>*/}
+                    <div className=" w-full min-w-[850px] h-[500px] lg:mx-auto lg:h-[850px]  m-0">
+                        <RiveWrapper src={"/rive/wdcc_designer_main.riv"} isPlaying={animEnabled} />
+                    </div>
+                    <div className="flex gap-4 flex-col sm:flex-row items-center justify-center">
+                        <Button variant={{ style: "primary", color: "yellow" }} href="https://forms.gle/o3jij89qCLMoDcr28">Project applications open now! <Arrow/></Button>
+                        <Button variant={{ style: "secondary", color: "light" }} href="/about/faq">Projects FAQ</Button>
+                    </div>
                 </div>
             </div>
+
+            <div className="flex flex-col gap-46 pt-2 pb-32 text-white">
+                <div className="flex flex-col gap-20">
+                    <div className="flex flex-col gap-8 text-center">
+                        <h2 className=" font-bold text-4xl leading-[1.1]">practice fantastic things.</h2>
+                        <div className="grid gap-12 sm:gap-8 md: gap-4 lg:grid-cols-3 sm:grid-cols-2">
+                            <LearnCard title={"teamwork"}
+                                       description={"producing something in a realistic agile tech team, making friends, and learning from each other."}
+                                       graphic={graphic1}/>
+                            <LearnCard title={"research"}
+                                       description={"understanding client needs, working with feedback, and participating in a designer-developer process."}
+                                       graphic={graphic2}/>
+                            <LearnCard title={"design"}
+                                       description={"make something beautiful for the world around you. improve your design theory, presentation skills, and creative craft."}
+                                       graphic={graphic3}/>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-28">
+                    <div className="flex flex-col gap-4 text-center">
+                        <h2 className=" font-bold text-4xl leading-none">go a step past theory.</h2>
+                        <p className="text-md leading-tight md:w-[75%] mx-auto">
+                            University tech curriculums will teach you the basics of code and craft, but projects let you apply those skills in the way lectures and tutorials can&apos;t.
+                        </p>
+                    </div>
+                    <RiveDuo rive={{ src: "/rive/wdcc_designer_2.riv", isPlaying: animEnabled }}>
+                        <h3 className="font-bold text-2xl leading-none">Design something real.</h3>
+                        <p className="text-md leading-tight">WDCC Projects aren’t just random assignments given for the
+                            sake
+                            of doing
+                            something.</p>
+                        <p className="text-md leading-tight">We only run projects with real-world clients, so you’ll
+                            deliver
+                            work that actually goes out into the wild - from new brand websites for huge university
+                            clubs to
+                            patient simulators for the Faculty of Medicine.</p>
+                    </RiveDuo>
+                    <RiveDuo imgFirst={true} rive={{ src: "/rive/wdcc_designer_3.riv", isPlaying: animEnabled }}
+                             largeAnim>
+                        <h3 className="font-bold text-2xl leading-none">Learn the tools of the trade.</h3>
+                        <p className="text-md leading-tight">WDCC teams use the same industry-standard tools and
+                            processes - from Figma and Jira to GitHub and Fly - as real companies do.</p>
+                        <p className="text-md leading-tight">You’ll be working in a realistic tech team following an
+                            Agile
+                            format with several other developers, leads, and project managers. You’ll learn to balance
+                            the
+                            designer-developer relationship and gain a practical understanding of design in a tech
+                            environment.</p>
+                    </RiveDuo>
+                    <RiveDuo  rive={{src: "/rive/wdcc_designer_4.riv", isPlaying: animEnabled}} largeAnim>
+                        <h3 className="font-bold text-2xl leading-none">Make your portfolio sparkle.</h3>
+                        <p className="text-md leading-tight">Project teams deliver apps, tools, and websites that have a real impact on your portfolio and CV.</p>
+                        <p className="text-md leading-tight">The products you work on get deployed into real-world use, helping hundreds of other students, staff, and community members do all kinds of things. </p>
+                        <p className="text-md leading-tight">At the end of the day, WDCC Projects are things you can point at in job interviews and say `&quot;I made that, and you can use it`&quot;.</p>
+                    </RiveDuo>
+                </div>
+
+                <MoreAboutProjects>
+                    <p className="text-md leading-tight">
+                        WDCC Projects are about more than design. Learn more about projects and how they work on these pages:
+                    </p>
+                </MoreAboutProjects>
+
+                <PastProjects/>
+
+                <SignupCard
+                    SignUpCard={{
+                        title: "Not a member yet?",
+                        descriptionLineOne: "Joining WDCC lets you stay updated on all of our workshops, events, and projects. It's also free!",
+                        descriptionLineTwo: "Plus, membership lets you participate in a "
+                    }} />
+
+            </div>
+
+
         </StandardPageLayout>
-    );
+    )
+        ;
 }
--- src/app/(home)/projects/roles/techlead/page.tsx
+++ src/app/(home)/projects/roles/techlead/page.tsx
@@ -1,88 +1,133 @@
 "use client";
 
-import { ArticleContent } from "@/components/article/ArticleContent";
-import Header from "@/components/layout/pageheaders/Header";
+import React from "react";
+
+import RolesHeader from "@app/projects/roles/_components/RolesHeader";
 import StandardPageLayout from "@/components/layout/StandardPageLayout";
 
+import { Button } from "@/components/primitives/Button";
+import SignupCard from "@app/_components/SignupCard";
+import LearnCard from "@app/projects/roles/_components/LearnCard";
+import RiveToggleButton from "@app/projects/roles/_components/RiveToggleButton";
+import RiveDuo from "@/components/rive/RiveDuo";
+import RiveWrapper from "@/components/rive/RiveWrapper";
+
+import Arrow from "@/assets/svg/Arrow"
+import MoreAboutProjects from "@app/projects/roles/_components/MoreAboutProjects";
+import PastProjects from "@app/projects/roles/_components/PastProjects";
+
+import graphic1 from "@/assets/image/roles/roles_design-01.webp";
+import graphic2 from "@/assets/image/roles/roles_design-02.webp";
+import graphic3 from "@/assets/image/roles/roles_design-03.webp";
+
+
 export default function TLPage() {
+
+    const [animEnabled, setAnimEnabled] = React.useState(true);
+
     return (
-        <StandardPageLayout>
-            <Header
-                variant={{ style: "secondary", color: "yellow" }}
-                title="Techlead in a WDCC project"
+        <StandardPageLayout className="bg-green-600" navColor="dark">
+
+            <RiveToggleButton animEnabled={animEnabled} setAnimEnabled={setAnimEnabled}/>
+
+            <RolesHeader
+                variant={{ color: "green" }}
+                title="Live your Lead SWE dream as a WDCC Tech Lead."
+                role="Tech Lead"
+                description1="The best web development projects don’t get there just because of code."
+                description2="As a designer in a WDCC Project, you’ll help craft beautiful, usable, and practical solutions to some of our community’s coolest tech challenges."
                 backlink={{ label: "projects", href: "/projects#roles" }}
             />
 
-            <div className="flex flex-col gap-24 py-24">
-                <ArticleContent variant={{ color: "yellow" }}>
-                    {/* replace with mdx or cms?too */}
-                    <h1 className="text-3xl font-bold" id="role-of-a-techlead">
-                        Role of a techlead
-                    </h1>
-                    <p>
-                        The primary responsibilities of a tech lead include technical leadership whereby they must have
-                        strong technical skills and be able to guide the team in making technical decisions, such as
-                        choosing the right technology stack, architecture, and design patterns. Furthermore, they are
-                        responsible for code reviews and providing feedback to developers, particularly at starting
-                        stages.
-                    </p>
-                    <h2 className="text-xl font-bold" id="tools-to-use">
-                        Tools to use
-                    </h2>
-                    <p>
-                        WDCC&apos;s project have a set list of technologies that we use across all our projects such as
-                        fly.io, github, github projects, github actions, figma, and google drive.
-                    </p>
-                    <p>
-                        Core techonologies for building the application such as databases, frontend frameworks, and
-                        backend frameworks are decided by project leads.
-                    </p>
-                    <p>The tech lead is responsible for leading the team, ensuring that the team is aligned with the</p>
-                    <h1 className="text-3xl font-bold" id="agile-and-meetings">
-                        Agile and Meetings
-                    </h1>
-                    <p>
-                        WDCC structures all project teams as agile scrum teams, with slight variations to the scrum
-                        methodology to work in a university context.
-                    </p>
-                    <p>
-                        Agile teams work in short, iterative cycles (2 weeks for WDCC) to deliver small increments of
-                        functionality that can be tested and validated by customers. Regular feedback and continuous
-                        improvement are key elements of agile, enabling teams to quickly adapt to changing requirements
-                        and priorities.
-                    </p>
-                    <h2 className="text-3xl font-bold" id="standups">
-                        Standups
-                    </h2>
-                    <p>
-                        At the start of all dev sessions, as well as other times where your team elects to run standups
-                        (either online, or via chat), the point is to get the PM/TL as well as other team members up to
-                        speed to where the team is up to. You will likely go through the following points:
-                    </p>
-                    <ul className="list-disc pl-6">
-                        <li>What progress have you made since the last meeting?</li>
-                        <li>What are you currently working on?</li>
-                        <li>Are you blocked by anything?</li>
-                        <li>Are you on track to finish your tasks by the end of the sprint?</li>
-                    </ul>
-                    <p>
-                        This should be really quick, roughly 10 - 15 minutes for the entire team and from
-                        everyone&apos;s responses PMs and TLs can then reorganise resources for areas that might need
-                        more help to meet sprint deadlines.
-                    </p>
-                    <h2 className="text-3xl font-bold" id="dev-sessions">
-                        Dev Sessions
-                    </h2>
-                    <p>
-                        This is where all the fun happens! Dev sessions (in the timeline) are just a block of time that
-                        everyone is available at, and is a chance for PMs and TLs to get up to date with everyone&apos;s
-                        progress. This is also a chance for you to work on your tasks, or get help from PM (relating to
-                        product queries) or the TL (relating to technical help). WDCC recommends teams to have time set
-                        aside per week to do some dev work so that you have a chance to peer-program / collaborate with
-                        other members of your team, and quickly get help if needed.
-                    </p>
-                </ArticleContent>
+            <div className="flex flex-col gap-42 pb-32 text-white responsive-fullwidth">
+                <div className="flex flex-col gap-2 justify-center items-center w-screen m-0">
+                    {/*<div className="h-[500px] rounded-2xl w-full bg-purple-300"></div>*/}
+                    <div className=" w-full min-w-[850px] h-[500px] lg:mx-auto lg:h-[850px]  m-0">
+                        <RiveWrapper src={"/rive/wdcc_designer_main.riv"} isPlaying={animEnabled} />
+                    </div>
+                    <div className="flex gap-4 flex-col sm:flex-row items-center justify-center">
+                        <Button variant={{ style: "primary", color: "yellow" }} href="https://forms.gle/o3jij89qCLMoDcr28">Project applications open now! <Arrow/></Button>
+                        <Button variant={{ style: "secondary", color: "light" }} href="/about/faq">Projects FAQ</Button>
+                    </div>
+                </div>
             </div>
+
+            <div className="flex flex-col gap-46 pt-2 pb-32 text-white">
+                <div className="flex flex-col gap-20">
+                    <div className="flex flex-col gap-8 text-center">
+                        <h2 className=" font-bold text-4xl leading-[1.1]">practice fantastic things.</h2>
+                        <div className="grid gap-12 sm:gap-8 md: gap-4 lg:grid-cols-3 sm:grid-cols-2">
+                            <LearnCard title={"teamwork"}
+                                       description={"producing something in a realistic agile tech team, making friends, and learning from each other."}
+                                       graphic={graphic1}/>
+                            <LearnCard title={"research"}
+                                       description={"understanding client needs, working with feedback, and participating in a designer-developer process."}
+                                       graphic={graphic2}/>
+                            <LearnCard title={"design"}
+                                       description={"make something beautiful for the world around you. improve your design theory, presentation skills, and creative craft."}
+                                       graphic={graphic3}/>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-28">
+                    <div className="flex flex-col gap-4 text-center">
+                        <h2 className=" font-bold text-4xl leading-none">go a step past theory.</h2>
+                        <p className="text-md leading-tight md:w-[75%] mx-auto">
+                            University tech curriculums will teach you the basics of code and craft, but projects let you apply those skills in the way lectures and tutorials can&apos;t.
+                        </p>
+                    </div>
+                    <RiveDuo rive={{ src: "/rive/wdcc_designer_2.riv", isPlaying: animEnabled }}>
+                        <h3 className="font-bold text-2xl leading-none">Design something real.</h3>
+                        <p className="text-md leading-tight">WDCC Projects aren’t just random assignments given for the
+                            sake
+                            of doing
+                            something.</p>
+                        <p className="text-md leading-tight">We only run projects with real-world clients, so you’ll
+                            deliver
+                            work that actually goes out into the wild - from new brand websites for huge university
+                            clubs to
+                            patient simulators for the Faculty of Medicine.</p>
+                    </RiveDuo>
+                    <RiveDuo imgFirst={true} rive={{ src: "/rive/wdcc_designer_3.riv", isPlaying: animEnabled }}
+                             largeAnim>
+                        <h3 className="font-bold text-2xl leading-none">Learn the tools of the trade.</h3>
+                        <p className="text-md leading-tight">WDCC teams use the same industry-standard tools and
+                            processes - from Figma and Jira to GitHub and Fly - as real companies do.</p>
+                        <p className="text-md leading-tight">You’ll be working in a realistic tech team following an
+                            Agile
+                            format with several other developers, leads, and project managers. You’ll learn to balance
+                            the
+                            designer-developer relationship and gain a practical understanding of design in a tech
+                            environment.</p>
+                    </RiveDuo>
+                    <RiveDuo  rive={{src: "/rive/wdcc_designer_4.riv", isPlaying: animEnabled}} largeAnim>
+                        <h3 className="font-bold text-2xl leading-none">Make your portfolio sparkle.</h3>
+                        <p className="text-md leading-tight">Project teams deliver apps, tools, and websites that have a real impact on your portfolio and CV.</p>
+                        <p className="text-md leading-tight">The products you work on get deployed into real-world use, helping hundreds of other students, staff, and community members do all kinds of things. </p>
+                        <p className="text-md leading-tight">At the end of the day, WDCC Projects are things you can point at in job interviews and say `&quot;I made that, and you can use it`&quot;.</p>
+                    </RiveDuo>
+                </div>
+
+                <MoreAboutProjects>
+                    <p className="text-md leading-tight">
+                        WDCC Projects are about more than design. Learn more about projects and how they work on these pages:
+                    </p>
+                </MoreAboutProjects>
+
+                <PastProjects/>
+
+                <SignupCard
+                    SignUpCard={{
+                        title: "Not a member yet?",
+                        descriptionLineOne: "Joining WDCC lets you stay updated on all of our workshops, events, and projects. It's also free!",
+                        descriptionLineTwo: "Plus, membership lets you participate in a "
+                    }} />
+
+            </div>
+
+
         </StandardPageLayout>
-    );
+    )
+        ;
 }
