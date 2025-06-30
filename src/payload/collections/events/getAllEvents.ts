d52bb7a168940550d4fc02e7238c2f7fefb86659
Oculux314 <nate.williamson.nz@gmail.com>
2025-06-30 13:57:36 +1200

    :sparkles: add speedy dev mock data for events

--- src/payload/collections/events/getAllEvents.ts
+++ src/payload/collections/events/getAllEvents.ts
@@ -1,15 +1,23 @@
 "use server";
 
+import { Event } from "@/payload/payload-types";
 import { SLUG } from "@/utils/enums/slug";
 import { getPayload } from "@/utils/payload";
 
 // return all events
 export const getAllEvents = async () => {
-    const payload = await getPayload();
-    const events = await payload.find({
-        collection: SLUG.EVENTS,
-        pagination: false,
-    });
-
-    return events.docs;
+    let events: Event[];
+    if (process.env.MOCK_DATA === "true") {
+        const { mockEventsData } = await import("./mockEventsData");
+        events = mockEventsData.docs;
+    } else {
+        const payload = await getPayload();
+        events = (
+            await payload.find({
+                collection: SLUG.EVENTS,
+                pagination: false,
+            })
+        ).docs;
+    }
+    return events;
 };
--- src/payload/collections/events/getEvent.ts
+++ src/payload/collections/events/getEvent.ts
@@ -1,20 +1,25 @@
 "use server";
 
-import { CollectionSlug } from "payload";
 import { Event } from "@/payload/payload-types";
 import { SLUG } from "@/utils/enums/slug";
 import { getPayload } from "@/utils/payload";
 
 // return Event
 export const getEvent = async (slug: string) => {
-    const payload = await getPayload();
-    const events = await payload.find({
-        collection: SLUG.EVENTS as CollectionSlug,
-        where: {
-            slug: { equals: slug },
-        },
-        limit: 1,
-    });
-
-    return events.docs[0] as Event;
+    let event: Event;
+    if (process.env.MOCK_DATA === "true") {
+        const { mockEventsData } = await import("./mockEventsData");
+        event = mockEventsData.docs[0];
+    } else {
+        const payload = await getPayload();
+        const events = await payload.find({
+            collection: SLUG.EVENTS,
+            where: {
+                slug: { equals: slug },
+            },
+            limit: 1,
+        });
+        event = events.docs[0];
+    }
+    return event;
 };
