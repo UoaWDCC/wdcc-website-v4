c12b2c6e7d14b77659baa3929ef47e34ae894c65
AntGa <antonvince.garay@gmail.com>
2025-07-22 14:03:06 +1200

    🚨 lint

--- src/app/(main)/projects/[proj]/_components/TeamMemberCard.tsx
+++ src/app/(main)/projects/[proj]/_components/TeamMemberCard.tsx
@@ -1,50 +1,50 @@
-import { tv, type VariantProps } from "tailwind-variants";
-import { ImageFit } from "@/components/primitives";
+import { type VariantProps, tv } from "tailwind-variants";
 import type { ImageType } from "@/types/misc/Image";
+import { ImageFit } from "@/components/primitives";
 import placeholder from "../_assets/placeholder.png";
 
 const card = tv({
-	base: "flex w-min flex-col gap-4 rounded-lg p-4",
-	variants: {
-		role: {
-			developer: "bg-blue-200",
-			techlead: "bg-green-200",
-			designer: "bg-purple-200",
-			manager: "bg-yellow-200",
-			tutor: "bg-red-200",
-		},
-	},
-	defaultVariants: {
-		role: "developer",
-	},
+    base: "flex w-min flex-col gap-4 rounded-lg p-4",
+    variants: {
+        role: {
+            developer: "bg-blue-200",
+            techlead: "bg-green-200",
+            designer: "bg-purple-200",
+            manager: "bg-yellow-200",
+            tutor: "bg-red-200",
+        },
+    },
+    defaultVariants: {
+        role: "developer",
+    },
 });
 
 type Variant = { variant?: VariantProps<typeof card> };
 
 type TeamCardProps = {
-	name: string;
-	role: string;
-	image?: ImageType;
+    name: string;
+    role: string;
+    image?: ImageType;
 } & Variant;
 
 const TeamMemberCard = ({ name, role, image, variant }: TeamCardProps) => {
-	return (
-		<div className={card(variant)}>
-			<div className="h-[150px] w-[200px] rounded-md bg-gray-50">
-				<ImageFit
-					src={image?.src ?? placeholder}
-					alt={image?.alt ?? name}
-					height={"150px"}
-					width="200px"
-					objectFit="cover"
-				/>
-			</div>
-			<div className="space-2">
-				<p className="font-semibold">{name}</p>
-				<p>{role}</p>
-			</div>
-		</div>
-	);
+    return (
+        <div className={card(variant)}>
+            <div className="h-[150px] w-[200px] rounded-md bg-gray-50">
+                <ImageFit
+                    src={image?.src ?? placeholder}
+                    alt={image?.alt ?? name}
+                    height={"150px"}
+                    width="200px"
+                    objectFit="cover"
+                />
+            </div>
+            <div className="space-2">
+                <p className="font-semibold">{name}</p>
+                <p>{role}</p>
+            </div>
+        </div>
+    );
 };
 
 export default TeamMemberCard;
--- src/components/primitives/ImageFit.tsx
+++ src/components/primitives/ImageFit.tsx
@@ -3,39 +3,29 @@ import Image, { type StaticImageData } from "next/image";
 import { cn } from "@/utils/misc";
 
 export type ImageFitProps = {
-	src: string | StaticImageData | StaticImport;
-	alt: string;
-	width?: string;
-	height?: string;
-	className?: string;
-	objectFit?: "cover" | "contain";
+    src: string | StaticImageData | StaticImport;
+    alt: string;
+    width?: string;
+    height?: string;
+    className?: string;
+    objectFit?: "cover" | "contain";
 };
 
-const ImageFit = ({
-	src,
-	alt,
-	width = "150px",
-	height = "80px",
-	className,
-	objectFit = "contain",
-}: ImageFitProps) => {
-	return (
-		<div
-			className={cn("", className)}
-			style={{ position: "relative", minWidth: width, minHeight: height }}
-		>
-			<Image
-				priority
-				fill
-				sizes="100%"
-				alt={alt}
-				src={src}
-				style={{
-					objectFit,
-				}}
-			/>
-		</div>
-	);
+const ImageFit = ({ src, alt, width = "150px", height = "80px", className, objectFit = "contain" }: ImageFitProps) => {
+    return (
+        <div className={cn("", className)} style={{ position: "relative", minWidth: width, minHeight: height }}>
+            <Image
+                priority
+                fill
+                sizes="100%"
+                alt={alt}
+                src={src}
+                style={{
+                    objectFit,
+                }}
+            />
+        </div>
+    );
 };
 
 export default ImageFit;
