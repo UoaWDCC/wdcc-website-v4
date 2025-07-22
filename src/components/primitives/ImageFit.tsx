01c151f3855b0ce787501a8bab3a50c0119cb5ce
AntGa <antonvince.garay@gmail.com>
2025-07-22 13:56:31 +1200

    ♻ merge ImageFit and ImageCover via prop usage

--- src/components/primitives/ImageFit.tsx
+++ src/components/primitives/ImageFit.tsx
@@ -1,30 +1,41 @@
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
-import Image, { StaticImageData } from "next/image";
+import type { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image, { type StaticImageData } from "next/image";
 import { cn } from "@/utils/misc";
 
 export type ImageFitProps = {
-    src: string | StaticImageData | StaticImport;
-    alt: string;
-    width?: string;
-    height?: string;
-    className?: string;
+	src: string | StaticImageData | StaticImport;
+	alt: string;
+	width?: string;
+	height?: string;
+	className?: string;
+	objectFit?: "cover" | "contain";
 };
 
-const ImageFit = ({ src, alt, width = "150px", height = "80px", className }: ImageFitProps) => {
-    return (
-        <div className={cn("", className)} style={{ position: "relative", minWidth: width, minHeight: height }}>
-            <Image
-                priority
-                fill
-                sizes="100%"
-                alt={alt}
-                src={src}
-                style={{
-                    objectFit: "contain",
-                }}
-            />
-        </div>
-    );
+const ImageFit = ({
+	src,
+	alt,
+	width = "150px",
+	height = "80px",
+	className,
+	objectFit = "contain",
+}: ImageFitProps) => {
+	return (
+		<div
+			className={cn("", className)}
+			style={{ position: "relative", minWidth: width, minHeight: height }}
+		>
+			<Image
+				priority
+				fill
+				sizes="100%"
+				alt={alt}
+				src={src}
+				style={{
+					objectFit,
+				}}
+			/>
+		</div>
+	);
 };
 
 export default ImageFit;
