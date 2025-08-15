950610289fa4666e8075eb9dfbad70c8888528e9
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2025-08-15 12:50:22 +1200

    :fire: :hammer: delete faq page fallback data and refactor payload fetch/parse

--- src/app/(main)/about/faq/_data/projectsFaq.data.ts
+++
@@ -1,8 +0,0 @@
-import { Faq } from "@/types/models/Faq";
-
-export const projectsFaqData: Faq[] = [
-    {
-        question: "What are WDCC Projects?",
-        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac purus sit amet nulla facilisi. Nullam euismod, nunc nec vehicula ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam euismod, nunc nec vehicula ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam euismod, nunc nec vehicula ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam euismod, nunc nec vehicula ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc.",
-    },
-];
--- src/app/(main)/about/faq/_data/teamFaq.data.ts
+++
@@ -1,8 +0,0 @@
-import { Faq } from "@/types/models/Faq";
-
-export const teamFaqData: Faq[] = [
-    {
-        question: "How do I become part of the WDCC exec team?",
-        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac purus sit amet nulla facilisi. Nullam euismod, nunc nec vehicula ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam euismod, nunc nec vehicula ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam euismod, nunc nec vehicula ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc. Nullam euismod, nunc nec vehicula ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc.",
-    },
-];
