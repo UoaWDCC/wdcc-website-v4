44cea771d14afb6768377fdbac648261aa731fcc
Oculux314 <nate.williamson.nz@gmail.com>
2025-08-15 18:27:52 +1200

    :fire: remove revalidation route

--- src/app/(main)/revalidate/revalidateAllRoutesAction.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { revalidatePath } from "next/cache";
-
-export async function revalidateAllRoutesAction() {
-    revalidatePath("/", "layout");
-    console.log("All routes revalidated at", new Date().toISOString());
-}
